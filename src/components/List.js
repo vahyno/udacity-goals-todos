e1103891cf48078c15607ed63e2204671168ef78
vahyno <vahyno@gmail.com>
2018-09-10 12:19:12 -0700

    created goals,list,todos components

---
+++ src/components/List.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function List (props) {
+    return (
+        <ul>
+            {props.items.map(item => {
+                return(
+                    <li key={item.id}>
+                        <span 
+                            onClick={() => props.toggle && props.toggle(item.id)}
+                            style={{textDecoration: item.complete ? 'line-through' : 'none'}}>
+                                {item.name}
+                        </span>
+                        <button onClick={()=> props.remove(item)}>
+                            X
+                        </button>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+export default List;
